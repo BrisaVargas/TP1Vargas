e9f658b3d00178f545524179de68f41197df9f09
varga <varga@DESKTOP-VMGS215>
2023-09-09 10:38:35 -0300

    Agregar archivos de proyecto.

---
+++ TrabajoPractico1Vargas/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TrabajoPractico1Vargas
+{
+    partial class EnvioDeCorreos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nick = new System.Windows.Forms.TextBox();
+            this.mail = new System.Windows.Forms.TextBox();
+            this.mensaje = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nick
+            // 
+            this.nick.Location = new System.Drawing.Point(26, 36);
+            this.nick.Name = "nick";
+            this.nick.Size = new System.Drawing.Size(100, 20);
+            this.nick.TabIndex = 1;
+            // 
+            // mail
+            // 
+            this.mail.Location = new System.Drawing.Point(184, 36);
+            this.mail.Name = "mail";
+            this.mail.Size = new System.Drawing.Size(100, 20);
+            this.mail.TabIndex = 2;
+            // 
+            // mensaje
+            // 
+            this.mensaje.Location = new System.Drawing.Point(336, 35);
+            this.mensaje.Multiline = true;
+            this.mensaje.Name = "mensaje";
+            this.mensaje.Size = new System.Drawing.Size(143, 100);
+            this.mensaje.TabIndex = 3;
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(122, 167);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(19, 13);
+            this.resultado.TabIndex = 4;
+            this.resultado.Text = "  . ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nickname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(355, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mensaje:";
+            // 
+            // EnvioDeCorreos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.mensaje);
+            this.Controls.Add(this.mail);
+            this.Controls.Add(this.nick);
+            this.Controls.Add(this.button1);
+            this.Name = "EnvioDeCorreos";
+            this.Text = "Envío de correos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nick;
+        private System.Windows.Forms.TextBox mail;
+        private System.Windows.Forms.TextBox mensaje;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TrabajoPractico1Vargas/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Formulario de contacto que tome un nickname, mail y mensaje. Al agregar debe anexarse a 
+ * los demás dados de alta (nickname, mail, 
+ * mensaje y fecha de envio de mensaje) visualizados en una Label.*/
+
+/*Vargas Maria Brisa*/
+
+namespace TrabajoPractico1Vargas
+{
+    
+    public partial class EnvioDeCorreos : Form
+    {
+        public EnvioDeCorreos()
+        {
+            InitializeComponent();
+        }
+        public string listado = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                DateTime fechaEnvio = DateTime.Today;
+                listado = listado + "Nickname: "+nick.Text + " - Mail: " + mail.Text + " - Mensaje: " + mensaje.Text + " (Fecha de envío: " + fechaEnvio.ToString("d") + ")\n------------\n";
+                resultado.Text = listado;
+                nick.Text = "";
+                mail.Text = "";
+                mensaje.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ingrese valores");
+            }
+        }
+    }
+}
